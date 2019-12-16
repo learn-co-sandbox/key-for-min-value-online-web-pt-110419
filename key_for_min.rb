a3d81d284c13a79b1d9bb2891b873c7fece88763
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-16 19:20:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,18 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
-
-end
+ # binding.pry
+mk = 0
+ name_hash.collect do |a_value|
+#  if mk == 1
+#    a_value
+#  elsif mk > 500 || mk == 500
+#  puts "you are too big"
+# elsif mk == 1 
+# puts "You are the smallest number"
+  
+#  name_hash -= 1 
+# end
+ end
+end 
