0a7c6dc3fc89c837b36642c6970c557544868edb
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-12-16 19:48:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,14 +6,16 @@ def key_for_min_value(name_hash)
 mk = 0
 return_key = nil
  name_hash.each do |key, a_value|
- if a_
-#    a_value
+ if a_value < mk
+   mk = a_value 
+   return_key = key
 #  elsif mk > 500 || mk == 500
 #  puts "you are too big"
 # elsif mk == 1 
 # puts "You are the smallest number"
   
-#  name_hash -= 1 
-# end
+# 
  end
+ end
+ return_key
 end 
